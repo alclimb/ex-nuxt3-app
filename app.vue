15a7a0f17f1af5685b54fcb4a8b748de965a7d71
hikaru <alclimb@users.noreply.github.com>
2021-10-13 09:38:39 +0900

    feat: first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test45
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
